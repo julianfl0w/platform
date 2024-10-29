a6deb9315bc3e9b3b36bf8b7728d5ade011dd246
Julian Loiacono <jcloiacon@gmail.com>
2024-10-29 13:33:47 -0400

    Delete go_bootstrap.sh

--- go_bootstrap.sh
+++
@@ -1,6 +0,0 @@
-bash install_go_1.22.sh
-/app/wait-for-it.sh keycloak:8888 --timeout=60 --strict -- \
-sh -c "
-  ./gobin/go/bin/go run ./service provision keycloak -e http://keycloak:8888/auth && \
-  ./gobin/go/bin/go run ./service start
-"
